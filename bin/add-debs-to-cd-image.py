5ed3d87a888b1bb722518775a1b257ed4ca2d535
Rory McCann <rory@technomancy.org>
2007-09-19 20:07:48 +0100

    releases file format added also added dict for names -> version strings

--- bin/add-debs-to-cd-image.py
+++ bin/add-debs-to-cd-image.py
@@ -19,6 +19,12 @@ dists_dir = [ dir for dir in os.listdir( os.path.join ( cddir, 'dists' ) ) if di
 assert len(dists_dir) == 1
 dist = dists_dir[0]
 
+dist_name_to_version = { 'warty':'4.10', 'hoary':'5.04', 'breezy':'5.10',
+                         'dapper':'6.06', 'edgy':'6.10', 'feisty':'7.04',
+                         'gusty':'7.10', 'hardy':'8.04' }
+
+assert dist in dist_name_to_version.keys()
+
 def makedir_if_not_exist( *path_parts ):
     path = os.path.join( *path_parts )
     if not os.path.exists( path ):
@@ -29,4 +35,8 @@ makedir_if_not_exist( cddir, 'pool', 'extras' )
 makedir_if_not_exist( cddir, 'isolinux' )
 makedir_if_not_exist( cddir, 'preseed' )
 
-print dist
+releases_file = open( os.path.join( cddir, 'dists', dist, 'extras', 'binary-i386', 'Release' ), 'w' )
+releases_file.write( "Archive: " + dist + "\n" )
+releases_file.write( "Version: " + dist_name_to_version[dist] + "\n" )
+releases_file.write( "Component: extras\nOrigin: Ubuntu\nLabel: Ubuntu\nArchitecture: i386\n")
+releases_file.close()
