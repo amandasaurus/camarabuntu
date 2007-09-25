1d828b9e022a22a47986275ce8acb8cc04a810da
Rory McCann <rory@technomancy.org>
2007-09-25 21:17:43 +0100

    Added the actual code and defaults

--- bin/make-cd-iso.py
+++ bin/make-cd-iso.py
@@ -5,17 +5,32 @@ from optparse import OptionParser
 parser = OptionParser()
 
 parser.add_option( "-f", "--iso-file",
-                   dest="isofile", metavar="FILE", type="string",
+                   dest="isofile", metavar="FILE", type="string", default=None,
                    help="The filename of the resultant ISO file" )
 
 parser.add_option( "-d", "--dir", "--directory",
-                   dest="dir", metavar="DIR", type="string",
+                   dest="dir", metavar="DIR", type="string", default=None,
                    help="The directory of the CD files" )
 
 parser.add_option( "-n", "--name",
-                   dest="name", metavar="NAME", type="string",
+                   dest="name", metavar="NAME", type="string", default=None,
                    help="The CD name" )
 
 (options, args) = parser.parse_args()
 
 
+if options.isofile is None or options.dir is None or options.name is None:
+    parser.print_help()
+
+import commands
+
+status, output = commands.getstatusoutput("""
+mkisofs -r -V "%s" \
+    -cache-inodes \
+    -J -l -b isolinux/isolinux.bin \
+    -c isolinux/boot.cat -no-emul-boot \
+    -boot-load-size 4 -boot-info-table \
+    -o "%s" "%s" """ % (options.name, options.isofile, options.dir ) )
+
+if status != 0:
+    print output
