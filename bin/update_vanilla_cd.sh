4d7cf320c2ed3a368da92b255631b0164a567acc
Tom Shortall <tshortall@arrakis.lmr.corp.google.com>
2007-09-27 14:56:45 +0100

    new script to automate process of merging our modified files from git into cd directory

---
+++ bin/update_vanilla_cd.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# automate the process of updating a vanilla edubuntu cd with our customisations
+# usage: $0 <path to git clone of camarabuntu project> <path to cd tree>
+
+camarabuntu_dir=$1
+cd_image_dir=$2
+
+set -x
+set -e
+
+ISO_DIRS="isolinux preseed"
+
+for dir in $ISO_DIRS ; do
+  cp -r $camarabuntu_dir/$dir/* $cd_image_dir/$dir/
+done
