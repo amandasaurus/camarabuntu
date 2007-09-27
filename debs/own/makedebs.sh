2a837dd85ba144468ce099934c5f01a8fd419963
Rory McCann <rory@athena.(none)>
2007-09-27 17:18:43 +0100

    Moved wikipedia to the own sub dir

--- debs/own/makedebs.sh
+++ debs/own/makedebs.sh
@@ -3,4 +3,5 @@ fakeroot dpkg-deb --build camara-backgrounds-0.1
 fakeroot dpkg-deb --build camara-classroom-aids-0.1
 fakeroot dpkg-deb --build camara-edu-guides-0.1
 fakeroot dpkg-deb --build camara-skillbuilder-0.1
+fakeroot dpkg-deb --build camara-wikipedia-0.1
 
