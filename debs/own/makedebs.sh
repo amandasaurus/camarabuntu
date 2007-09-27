35195fc2b5aba5e2dea27d2cf49e8b369851cbce
Rory McCann <rory@athena.(none)>
2007-09-27 17:18:10 +0100

    Moved skillbuilder into own

--- debs/own/makedebs.sh
+++ debs/own/makedebs.sh
@@ -2,4 +2,5 @@
 fakeroot dpkg-deb --build camara-backgrounds-0.1
 fakeroot dpkg-deb --build camara-classroom-aids-0.1
 fakeroot dpkg-deb --build camara-edu-guides-0.1
+fakeroot dpkg-deb --build camara-skillbuilder-0.1
 
