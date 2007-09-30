66a53cc5fe53a6246066b8a675120d28223e6f8c
Rory McCann <rory@technomancy.org>
2007-10-01 00:54:06 +0100

    Some refactoring for the goal of looking for |'s in depends lines

--- bin/get-deb-dependencies.py
+++ bin/get-deb-dependencies.py
@@ -1,7 +1,7 @@
 #! /usr/bin/python
 
 from optparse import OptionParser
-import os, re
+import os, re, commands
 
 parser = OptionParser()
 
@@ -17,39 +17,74 @@ deb = debs[0]
 assert len(options.repos) > 0
 
 class Dependency():
-    def __init__(self, package_name, version, relation):
-        self.package_name = package_name
-        self.version = version
-        self.relation = relation
+    def __init__(self, string=None, package_name=None, version=None, relation=None):
+        if string is not None:
+            depends_re = re.compile( r"(?P<package_name>\S*) \((?P<relation><<|<=|=|>=|>>) (?P<version>\S*)\)" )
+            result = depends_re.search( string )
+            if result is None:
+                self.package_name = string
+                self.version = None
+                self.relation = None
+            else:
+                self.package_name = result.group('package_name')
+                self.version = result.group('version')
+                self.relation = result.group('relation')
+        else:
+            self.package_name = package_name
+            self.version = version
+            self.relation = relation
 
     def __str__(self):
-        return "%s (%s %s)" % (self.package_name, self.relation, self.version)
+        if self.relation is None and self.version is None:
+            return self.package_name
+        else:
+            return "%s (%s %s)" % (self.package_name, self.relation, self.version)
 
     def __repr__(self):
-        return "Dependency( %r, %r, %r )" % (self.package_name, self.version, self.relation )
+        return "Dependency( package_name=%r, version=%r, relation=%r )" % (self.package_name, self.version, self.relation )
 
 class Package():
-    def __init__(self):
+    def __init__(self, filename=None):
         self.name = None
         self.version = None
         self.depends_line = None
-        pass
+
+        if filename is not None:
+            self.__init_from_deb_file(os.path.abspath(filename))
+
+    def __init_from_deb_file(self, filename):
+        assert os.path.isfile(filename)
+
+        status, output = commands.getstatusoutput("dpkg-deb --info \"%s\"" % filename)
+        if status != 0:
+            print output
+            return
+
+        for line in output.split("\n"):
+            # remove the leading space
+            line = line[1:]
+            line = line.rstrip()
+            if line[0] == " ":
+                continue
+            if line.startswith("Depends: "):
+                key, value = line.split( ": ", 1 )
+                self.parse_dependencies( value )
+
 
     def parse_dependencies(self, depends_line):
-        self.depends = []
+        self.depends = self.parse_dependencies_rec( depends_line )
 
+
+    def parse_dependencies_rec( self,  depends_line ):
+        depends = []
+
+        # Assuming that "," is more important than "|"
         packages = depends_line.split( "," )
-        depends_re = re.compile( r"(?P<package_name>\S*) \((?P<relation><<|<=|=|>=|>>) (?P<version>\S*)\)" )
         for package in packages:
             package = package.strip()
-            result = depends_re.search(package)
-            if result is None:
-                # plain old depend
-                self.depends.append( package )
-            else:
-                self.depends.append( Dependency( result.group('package_name'), result.group('version'), result.group('relation') ) )
-
-        print repr( self.depends )
+            depends.append( Dependency( string = package ) )
+        
+        return depends
 
 
 class Repository():
@@ -86,4 +121,10 @@ class Repository():
 
 for repo in options.repos:
     r = Repository(repo)
+
+package = Package( filename=deb )
+print repr(package.depends)
+print ", ".join([str(dep) for dep in package.depends])
+
+
         
