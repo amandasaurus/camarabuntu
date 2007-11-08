d27043deb1285222429c799d6cce44beb0cc3773
Rory McCann <rory@athena.(none)>
2007-11-08 18:54:41 +0000

    apt-move runs into the right directories

--- bin/make-apt-cdrom.py
+++ bin/make-apt-cdrom.py
@@ -14,8 +14,39 @@ parser.add_option( "-n", "--cd-name",
                    dest="name", default=None,
                    help="The name of the CD" )
 
+parser.add_option( "-d", "--deb-dir",
+                   dest="debdir", default=None,
+                   help="The directory that has all the debs" )
 
-(options, debs) = parser.parse_args()
+
+(options, unneeded) = parser.parse_args()
+
+debdir = os.path.abspath( options.debdir )
+
+# this is the CD we work in and will have be the cd image layout
+apt_cdrom_dir = tempfile.mkdtemp( prefix="apt-cdrom-", dir=os.getcwd() )
+
+# this is our apt-move config file
+apt_config, apt_config_filename = tempfile.mkstemp( prefix="apt-move.conf-", dir=os.getcwd() )
+# TODO dist
+open( apt_config_filename, "w" ).write("""APTSITES="/all/"
+LOCALDIR=%(apt_cdrom_dir)s
+DIST=dapper
+PKGTYPE=binary
+FILECACHE=%(debdir)s
+LISTSTATE=/var/lib/apt/lists
+DELETE=no
+MAXDELETE=20
+COPYONLY=yes
+PKGCOMP=gzip
+CONTENTS=no
+GPGKEY=
+""" % {'debdir':debdir, 'apt_cdrom_dir':apt_cdrom_dir} )
+
+status, output = commands.getstatusoutput("apt-move -c \"%s\" update" % apt_config_filename)
+if status != 0:
+    print output
+assert status == 0
 
 # details:
 # https://help.ubuntu.com/community/AptMoveHowto
