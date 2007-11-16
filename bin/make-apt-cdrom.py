e0e4ac5c2a70fe965e91dc4244c41c8a18b7ea8f
Rory McCann <rory@athena.(none)>
2007-11-16 16:24:28 +0000

    + some TODO commenting

--- bin/make-apt-cdrom.py
+++ bin/make-apt-cdrom.py
@@ -61,6 +61,7 @@ if status != 0:
 
 # release file:
 release_file, release_filename = tempfile.mkstemp( prefix="release-", dir=os.getcwd() )
+# TODO remove tha 'apt-move' stuff. What else should it be called
 open( release_filename, "w" ).write("""APT::FTPArchive::Release {
 Origin "APT-Move";
 Label "APT-Move";
